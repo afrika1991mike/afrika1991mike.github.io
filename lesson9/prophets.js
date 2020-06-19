f1004d145e46cf2e92fb9a262a3ecef8e24182e9
afrika1991mike <afrikamike10@gmail.com>
2020-06-19 21:02:31 +0300

    commit

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -9,26 +9,22 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ){
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let p1= document.createElement('p');
-        let birthplace= document.createElement('birthplace');
-        let image= document.createElement('img');
-
-        
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p1.innerHTML ="<strong>"+ prophets[i].birthdate +"</strong>";
-        birthplace.textContent=prophets[i].birthplace;
-        image.setAttribute('src', prophets[i].imageurl);
-        
-        card.appendChild(h2);
-        card.appendChild(p1);
-        card.appendChild(birthplace)
-        card.appendChild(image)
-        
-        document.querySelector('div.cards').appendChild(card);
-    }
-
-  });
+    for (let i = 0; i < prophets.length; i++) { 
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let birthdate = document.createElement('h5');
+      let birthplace = document.createElement('h5');
+      let image = document.createElement('img');
   
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      birthdate.textContent = prophets[i].birthdate;
+      birthplace.textContent = prophets[i].birthplace;
+      image.setAttribute('src', prophets[i].imageurl);
+      card.appendChild(h2);
+      card.appendChild(birthdate);
+      card.appendChild(birthplace);
+      card.appendChild(image);
+      card.className = "card";
+  
+      document.querySelector('div.cards').appendChild(card);
+  }});
